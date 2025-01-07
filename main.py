89f173af94bffbbbe567360f30f613ac1e7a3569
DAOUDA <daoraf01@gmail.com>
2025-01-07 22:12:42 +0100

    version6

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from fonction import *
 
 print(verifcation())
 dataframe = load_data()
+print(dataframe.head())  # Display the first few rows of the DataFrame
+print(dataframe.columns)
+print(dataframe.shape)
 print(dataframe.info())
 
 new_data = visualisation_donnees_manquantes(dataframe)
+print(new_data.shape)
+print(new_data.columns)
 
