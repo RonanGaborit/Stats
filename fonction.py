863817a68bee24618c720ada5877d6fe5651b686
DAOUDA <daoraf01@gmail.com>
2025-01-09 17:19:00 +0100

    ajout de fonction2

--- fonction.py
+++ fonction.py
@@ -59,5 +59,17 @@ def visualisation_donnees_manquantes(dataframe):
 
 
 def verifcation():
-    nom_auteur = ["DAOUDA"]
+
+    nom_auteur = ["DAOUDA", "Khadija"]
     return nom_auteur
+
+
+def setup_plot():
+    from aquarel import load_theme
+    import seaborn as sns
+
+    theme = load_theme("minimal_light")
+    theme.apply()
+
+    return ["#004aad", "#2bb4d4", "#2e2e2e", "#5ce1e6"]
+
